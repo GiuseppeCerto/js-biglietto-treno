04126127b168b535f2e8849e9a1598cb616f8b9f
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-01-31 16:31:00 +0100

    EDIT disccounted price

--- js/app.js
+++ js/app.js
@@ -1,21 +1,36 @@
 // 1.chiedere nome passeggero
 let firstName = prompt ('Quale è il suo nome?')
 console.log(firstName)
+
 // 2.chiedere cognome passeggero
 let secondName = prompt ('Quale è il suo cognome?')
 console.log(secondName)
+
 // 3.chiedere età passeggero
 let ageClient = parseInt(prompt ('Quanti anni ha?'))
 console.log(ageClient)
+
 // 4.chiedere chilometri da percorrere
 let kmTravelled = parseInt(prompt('Quanti chilometri percorrerà?'))
 console.log(kmTravelled)
+
 // 5.moltiplicare i chilometri percorrenza per il prezzo (0,21 cent)
-let kmPrice = kmTravelled * 0.21 + '€'
-console.log(kmPrice)
+let totPrice = kmTravelled * 0.21
+console.log(totPrice)
+
 // applicare sconto in caso di minore eta o ultra 65
-let discont
-if ageClient > 65 {
-    kmPrice = kmTravelled * 0.21 - 40% =
+let discountedPrice
+
+let discountOver65 = 0.6
+
+let discountUnderage = 0.8
+
+if (ageClient > 65) {
+    console.log(discountedPrice = totPrice * 0.6)
 }
+
+if (ageClient < 18) {
+    console.log(discountedPrice = totPrice * 0.8)
+}
+
 // calcolare prezzo totale (km x 0,21 - % sconto ) senza numeri decimali infiniti ma solo con euro e centesimi
